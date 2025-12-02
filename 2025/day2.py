c20b1b6d2b0d50f837a0ec257b18d97ac57f74f8
0xlia <hi@0xlia.com>
2025-12-02 19:57:08 +0100

    2025 day2 unfertig

---
+++ 2025/day2.py
@@ -0,0 +1,22 @@
+sum_invalid_ids = 0
+
+# elfen sind stupid und haben database zerschossen 
+# wir müssen invalid IDs finden
+# invalid id: 446446 <- erster part der id entspricht dem 2. part
+
+
+with open("input2.txt") as f:
+    # parse puzzle input 
+    ranges = f.read().split(",")
+
+    # iterate over list of ranges
+    for ra in ranges:
+        # get first and last id
+        ids = ra.split("-")
+        first_id = int(ids[0])
+        last_id = int(ids[1])
+
+        # iterate over ids
+        i = first_id
+        for i in range(last_id+1):
+            pass
